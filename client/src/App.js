b89f5b08a1bb0f14b227bb3a86011845258d64b6
Denysse Cunza <33096931+denyssecunza@users.noreply.github.com>
2021-04-10 22:10:52 -0400

    feature-createLoginSignup (#21)
    * adds login and signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { BrowserRouter, Switch, Route, useParams } from "react-router-dom";
 import Home from "./Components/Home"
+import Login from "./Components/auth/Login"
+import Signup from "./Components/auth/Signup"
 
 function App () {
   return (
@@ -8,6 +10,8 @@ function App () {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
         </Switch>
       </BrowserRouter>
     </div>

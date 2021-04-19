6cb80f178d70ebc3a56e897b51bc6d4b1d0497c4
Jendri <jmorocho95@yahoo.com>
2021-04-19 15:15:49 -0400

    implement fetch from donation requests

--- client/src/Components/profiles/UserProfile.js
+++ client/src/Components/profiles/UserProfile.js
@@ -1,14 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Typography, Box } from "@material-ui/core";
 import { UserContext } from "../../contexts/UserContext";
-import { makeStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper'
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const styles = makeStyles({
   container: {
@@ -40,85 +40,138 @@ const styles = makeStyles({
   },
   table: {
     minWidth: 650,
-  }
+  },
 });
 
 const UserProfile = () => {
-  
-  const classes = styles()
+  const [userDonations, setDonations] = React.useState([])
 
+  const PROD = true;
+
+  const URL = PROD
+    ? "https://sharity-technyc.herokuapp.com"
+    : "http://localhost:3000";
+
+  const classes = styles();
 
   const sessionUser = useContext(UserContext);
   console.log(sessionUser.user, "user");
-  if(!sessionUser.user.avatar) {
-    sessionUser.user.avatar = "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+  if (!sessionUser.user.avatar) {
+    sessionUser.user.avatar =
+      "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png";
   }
 
-  // Temp data creation
-  function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+  // grab donation requests made by user
+  useEffect(() => {
+    // fetch user donation requests
+
+    fetch(`${URL}/api/user/fetch_requests`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        id: sessionUser.user.id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setDonations(data);
+      });
+
+  }, []);
+
+  // data parsing
+  function createData(request_number, organization_id, date, time, items, status) {
+    /**
+     * Status mapping
+     *  1 = pending
+     *  2 = accepted
+     *  3 = rejected
+     *  4 = completed
+     *
+     * */
+
+    if (status === 1) {
+      status = "Pending";
+    } else if (status === 2) {
+      status = "Accepted";
+    } else if (status === 3) {
+      status = "Rejected";
+    } else if (status === 4) {
+      status = "Completed";
+    }
+    return { request_number, organization_id, date, time, items, status };
   }
 
-  const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-
+  const rows = userDonations.map(donation => {
+    return createData(donation.id, donation.organization_id, donation.date, donation.time, donation.items, donation.status)
+  })
 
   return (
     <div>
       <Box container display="flex" style={{ paddingTop: "5%" }}>
-          <Box flexDirection="row" flexWrap="wrap" xs={2}>
-            <img
-              alt="avatar"
-              style={{ paddingLeft: "10%", width: "75%", height: "75%" }}
-              src={sessionUser.user.avatar}
-              referrerPolicy="no-referrer" 
-            />
-          </Box>
-          <Box flexDirection="row" item xs={4} direction="column">
-            <Typography variant="h6"> Username </Typography>
-            <Typography className={classes.textField} variant="h4">
-              {sessionUser.user.username}
-            </Typography>
-            <Typography variant="h6"> Email </Typography>
-            <Typography className={classes.textField} variant="h4">{sessionUser.user.email} </Typography>
-            <Typography variant="h6">Phone Number</Typography>
-            <Typography className={classes.textField} variant="h4">{sessionUser.user.phone_number} </Typography>
-          </Box>
-          <Box flexDirection="row" flexWrap="wrap" item xs={3} direction="column"></Box>
+        <Box flexDirection="row" flexWrap="wrap" xs={2}>
+          <img
+            alt="avatar"
+            style={{ paddingLeft: "10%", width: "75%", height: "75%" }}
+            src={sessionUser.user.avatar}
+            referrerPolicy="no-referrer"
+          />
+        </Box>
+        <Box flexDirection="row" item xs={4} direction="column">
+          <Typography variant="h6"> Username </Typography>
+          <Typography className={classes.textField} variant="h4">
+            {sessionUser.user.username}
+          </Typography>
+          <Typography variant="h6"> Email </Typography>
+          <Typography className={classes.textField} variant="h4">
+            {sessionUser.user.email}{" "}
+          </Typography>
+          <Typography variant="h6">Phone Number</Typography>
+          <Typography className={classes.textField} variant="h4">
+            {sessionUser.user.phone_number}{" "}
+          </Typography>
+        </Box>
+        <Box
+          flexDirection="row"
+          flexWrap="wrap"
+          item
+          xs={3}
+          direction="column"
+        ></Box>
       </Box>
       <hr />
-      <Box>
+      <Box container display="flex" style={{ paddingTop: "2%", padding: "5%" }}>
         <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Donation Request Number</TableCell>
+                <TableCell alight="left">Donating To</TableCell>
+                <TableCell align="right">Date</TableCell>
+                <TableCell align="right">Time</TableCell>
+                <TableCell align="left">Description</TableCell>
+                <TableCell align="right">Request Status</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.request_number}>
+                  <TableCell component="th" scope="row">
+                    {row.request_number}
+                  </TableCell>
+                  <TableCell align="center">{row.organization_id}</TableCell>
+                  <TableCell align="right">{row.date}</TableCell>
+                  <TableCell align="right">{row.time}</TableCell>
+                  <TableCell align="right">{row.items}</TableCell>
+                  <TableCell align="center">{row.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </div>
   );

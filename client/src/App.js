342fdf60bd46df969758195a53a4b60f75da5ae3
Pitom Saha <pitoms@gmail.com>
2021-04-20 17:14:26 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -70,7 +70,7 @@ function App() {
             <Route path="/terms" component={Terms} />
             <Route path="/contact" component={Contact} />
             <Route path="/dashboard" component={OrgProfile} />
-            <Route path="/profile"><UserOrg /></Route>
+            <Route path="/profile"><OrgProfile /></Route>
             <OrgContext.Provider value={orgName}>
               <Route path="/:value" exact render={(props) => <UserViewOrg {...props} />} />
             </OrgContext.Provider>

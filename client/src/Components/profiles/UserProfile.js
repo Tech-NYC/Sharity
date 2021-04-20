2066719f8e7e7dcd00a72311626f8290b6edab09
Pitom Saha <pitoms@gmail.com>
2021-04-20 15:36:43 -0400

    Update UserProfile.js

--- client/src/Components/profiles/UserProfile.js
+++ client/src/Components/profiles/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Typography, Box } from "@material-ui/core";
-import { UserContext } from "../../contexts/UserContext";
+import { UserContext } from "../../contexts/UserContext.js";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";

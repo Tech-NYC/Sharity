db8a4a10cffcd13d1a2e1db1498a481c9b04ccbc
Jendri <jmorocho95@yahoo.com>
2021-04-21 13:50:38 -0400

    change prod variable to true

--- client/src/Components/profiles/EditModal.js
+++ client/src/Components/profiles/EditModal.js
@@ -18,7 +18,7 @@ const btntheme = createMuiTheme({
 });
 
 export default function EditModal(props) {
-  const PROD = false;
+  const PROD = true;
   const URL = PROD ? "https://sharity-technyc.herokuapp.com" : "http://localhost:3000";
 
   const user = useContext(UserContext);

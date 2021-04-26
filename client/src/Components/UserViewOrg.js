b5c773e868458b57a47229ce8908db9dab178c7e
Pitom <pitoms@gmail.com>
2021-04-26 01:00:08 -0400

    center changes on uservieworg

--- client/src/Components/UserViewOrg.js
+++ client/src/Components/UserViewOrg.js
@@ -149,59 +149,61 @@ function UserViewOrg(props) {
   let daysArr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   return (
     <>
-      <Box container display="flex" style={{ paddingTop: "5%", paddingBottom: "5%" }}>
-        {mergedArray &&
-          mergedArray.map((data) => (
-            <>
-              <Box flexDirection="column" flexWrap="wrap" paddingLeft="5%" xs={2}>
-                <img alt="logo" src={data.logo} style={{ width: "100%" }} referrerPolicy="no-referrer" />
-              </Box>
-              <Box key={data.id} flexDirection="column" item xs={4} direction="column">
-                <Typography variant="h3" style={{ margin: "1%" }}>
-                  {data.name}
-                </Typography>
-                <Typography variant="h6" style={{ margin: "1%" }}>
-                  Address{" "}
-                </Typography>
-                <Typography variant="subtitle1" style={{ margin: "1%" }}>
-                  {data.address}
-                </Typography>
-                <Typography variant="h6" style={{ margin: "1%" }}>
-                  Description
-                </Typography>
-                <Typography variant="subtitle1" style={{ margin: "1%" }}>
-                  {data.description}
-                </Typography>
-                <Typography variant="h6" style={{ margin: "1%" }}>
-                  Pickup Times
-                </Typography>
-                <Typography variant="subtitle1" style={{ margin: "1%" }}>
-                  {!data.pickup ? (
-                    <Typography>No Times Yet</Typography>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <Box container display="flex" style={{ paddingTop: "5%", paddingBottom: "5%" }}>
+          {mergedArray &&
+            mergedArray.map((data) => (
+              <>
+                <Box flexDirection="column" flexWrap="wrap" paddingLeft="5%" xs={2}>
+                  <img alt="logo" src={data.logo} style={{ width: "100%" }} referrerPolicy="no-referrer" />
+                </Box>
+                <Box key={data.id} flexDirection="column" item xs={4} direction="column">
+                  <Typography variant="h3" style={{ margin: "1%" }}>
+                    {data.name}
+                  </Typography>
+                  <Typography variant="h6" style={{ margin: "1%" }}>
+                    Address{" "}
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ margin: "1%" }}>
+                    {data.address}
+                  </Typography>
+                  <Typography variant="h6" style={{ margin: "1%" }}>
+                    Description
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ margin: "1%" }}>
+                    {data.description}
+                  </Typography>
+                  <Typography variant="h6" style={{ margin: "1%" }}>
+                    Pickup Times
+                  </Typography>
+                  <Typography variant="subtitle1" style={{ margin: "1%" }}>
+                    {!data.pickup ? (
+                      <Typography>No Times Yet</Typography>
+                    ) : (
+                      data.pickup.split(",").map((time, i) => (
+                        <>
+                          <li style={{ listStyleType: "none" }} key={i}>
+                            {" "}
+                            {daysArr[i]} {formatTime(time.split("-")[0]) + " - " + formatTime(time.split("-")[1])}{" "}
+                          </li>
+                        </>
+                      ))
+                    )}
+                  </Typography>
+                </Box>
+                <Box flexDirection="column" item xs={4} direction="column" style={{ paddingRight: "5%" }}>
+                  {userLoggedIn ? (
+                    <ScheduleModal org_id={orgId}></ScheduleModal>
                   ) : (
-                    data.pickup.split(",").map((time, i) => (
-                      <>
-                        <li style={{ listStyleType: "none" }} key={i}>
-                          {" "}
-                          {daysArr[i]} {formatTime(time.split("-")[0]) + " - " + formatTime(time.split("-")[1])}{" "}
-                        </li>
-                      </>
-                    ))
+                    <Button align="center" component={Link} to="/login" variant="outlined" style={{ margin: "auto" }}>
+                      Request Pickup
+                    </Button>
                   )}
-                </Typography>
-              </Box>
-              <Box flexDirection="column" item xs={4} direction="column" style={{ paddingRight: "5%" }}>
-                {userLoggedIn ? (
-                  <ScheduleModal org_id={orgId}></ScheduleModal>
-                ) : (
-                  <Button align="center" component={Link} to="/login" variant="outlined" style={{ margin: "auto" }}>
-                    Request Pickup
-                  </Button>
-                )}
-              </Box>
-            </>
-          ))}
-      </Box>
+                </Box>
+              </>
+            ))}
+        </Box>
+      </div>
       <hr></hr>
       {orgNeeds.length === 0 ? (
         <Box container display="flex" justify="center" align="center" style={{ width: "1200px", overflow: "hidden", margin: "auto", padding: "25px" }}>
